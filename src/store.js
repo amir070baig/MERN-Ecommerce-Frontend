a2a872b58a09e25fc652637b0543b4ebcb0106f0
AmirTechDev <amirbaig070@gmail.com>
2025-09-02 14:14:38 +0530

    fixed file path name

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 // 1. Import configureStore from Redux Toolkit
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlices";
+import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
 import orderReducer from "./slices/orderSlice"
